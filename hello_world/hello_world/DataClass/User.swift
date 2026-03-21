01cad2ad5ba63e0ab4e93bd737a3f5ad90dbc43a
Alumnos <alumnos@D104MAC00-3.local>
2026-03-20 20:18:01 -0600

    first commit

---
+++ hello_world/hello_world/DataClass/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  hello_world
+//
+//  Created by Alumnos on 20/03/26.
+//
+
+import Foundation
+
+struct User {
+    var name: String = ""
+    var password: String = ""
+    var userName: String = ""
+}
---
+++ hello_world/hello_world/Scenes/Main/Model/MainModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainModel.swift
+//  hello_world
+//
+//  Created by Alumnos on 20/03/26.
+//
+
+import Foundation
+
+struct MainModel {
+    var user: User = .init()
+}

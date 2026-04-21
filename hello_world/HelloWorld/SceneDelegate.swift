987540be47d40e64e0741d501e26d9081994204d
andrulokuras07 <andres.angulo@anahuac.mx>
2026-04-20 20:28:08 -0600

    ya todo

--- hello_world/HelloWorld/SceneDelegate.swift
+++ hello_world/HelloWorld/SceneDelegate.swift
@@ -16,11 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let hello = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: hello)
-        window.overrideUserInterfaceStyle = .dark
-        self.window = window
-        window.makeKeyAndVisible( )
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
